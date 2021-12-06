18b1e2d9f0352efeb06cc5c1162e79c8150efbaf
Max Velikanov <maximelianos.m@gmail.com>
2021-12-06 23:13:21 +0300

    1

--- yolov3-master/polus_scripts/submit.sh
+++ yolov3-master/polus_scripts/submit.sh
@@ -1,3 +1,6 @@
-bsub -o job_stdout.txt \
+rel_path=$(dirname "$0")                # relative
+abs_path=$(cd "$rel_path" && pwd)       # absolutized and normalized
+
+cd "$rel_path" && bsub -o job_stdout.txt \
     -e job_stderr.txt -W 00:01 -q normal -gpu "num=1:mode=exclusive_process" \
     bash start_net.sh

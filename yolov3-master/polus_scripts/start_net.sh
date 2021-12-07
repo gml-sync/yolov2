11a505713f17aabae48e65c4819d01f045ad0648
Max Velikanov <maximelianos.m@gmail.com>
2021-12-07 14:56:11 +0300

    1

--- yolov3-master/polus_scripts/start_net.sh
+++ yolov3-master/polus_scripts/start_net.sh
@@ -1,4 +1,4 @@
 rel_path=$(dirname "$0")                # relative
 abs_path=$(cd "$rel_path" && pwd)       # absolutized and normalized
 
-cd "$rel_path/.." && python train.py --weights models/yolov3.pt > >(tee logfile) 2>&1 
+cd "$rel_path/.." && python train.py --weights models/yolov3.pt --data coco.yaml > >(tee logfile) 2>&1 

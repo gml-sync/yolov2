9dc32b60e4053c86e32162b116f13c91a5efa608
Max Velikanov <maximelianos.m@gmail.com>
2021-12-07 14:36:48 +0300

    1

--- yolov3-master/utils/plots.py
+++ yolov3-master/utils/plots.py
@@ -151,7 +151,7 @@ def save_intermediate(x, module_type="", stage=0, n=32, save_dir=Path('runs/dete
                     f = f"{stage}_{batch_i}_features.png"  # filename
                     image = x[batch_i].cpu()
                     #LOGGER.info(f"\nSaving features... DTYPE {x.dtype} SIZE {x.size()} MIN {x.min()} MAX {x.max()}")
-                    image = einops.rearrange(x, '(i1 i2) h w -> (i1 h) (i2 w)', i1=16) # 16 is specific to layer 5
+                    image = einops.rearrange(image, '(i1 i2) h w -> (i1 h) (i2 w)', i1=16) # 16 is specific to layer 5
                     #LOGGER.info(f"\nSaving features... DTYPE {x.dtype} SIZE {x.size()} MIN {x.min()} MAX {x.max()}")
                     image = (image - image.min()) / (image.max() - image.min() + 0.0001) # set value range to [0, 1]
                     cv2.imwrite(str(save_dir / f), (image.numpy() * 255).astype(np.uint8))

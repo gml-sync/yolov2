96a24ba163c73cf631d1df0ad94b35f5b5d73d57
Max Velikanov <maximelianos.m@gmail.com>
2021-12-14 15:10:31 +0300

    1

--- yolov3-master/polus_scripts/submit.sh
+++ yolov3-master/polus_scripts/submit.sh
@@ -3,4 +3,4 @@ abs_path=$(cd "$rel_path" && pwd)       # absolutized and normalized
 
 cd "$rel_path" && bsub -o job_stdout.txt \
     -e job_stderr.txt -W 00:01 -q normal -gpu "num=1:mode=exclusive_process" \
-    bash start_net.sh 
+    bash train_restore.sh 

46e9667ed981af9b393a46ca39b53080ddedf790
Max Velikanov <maximelianos.m@gmail.com>
2021-12-14 14:59:54 +0300

    1

--- yolov3-master/train_restore.py
+++ yolov3-master/train_restore.py
@@ -47,7 +47,241 @@ class RestorationDataset(data.Dataset):
 train_dataset = RestorationDataset()
 f, i = train_dataset[0]
 print(f.shape, i.shape)
-print(train_dataset.feature_list[:5],
-    train_dataset.image_list[:5],
-    train_dataset.desc_list[:5])
+print(train_dataset.feature_list[1000],
+    train_dataset.image_list[1000],
+    train_dataset.desc_list[1000])
 
+class RestorationDecoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.up1 = torch.nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bottle1_1 = self.bottleneck_block(128)
+        self.bottle1_2 = self.bottleneck_block(128)
+
+        self.up2 = torch.nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bottle2 = self.bottleneck_block(64)
+
+        self.up3 = torch.nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.conv_out = torch.nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+
+    def __call__(self, x):
+        # print(f"x: {x.size()}")
+        up1 = self.up1(x)
+        # print(f"up1: {up1.size()}")
+        p = self.bottle1_1(up1)
+        # print(f"bottle1_1: {p.size()}")
+        bottle1_1 = self.bottle1_1(up1) + up1
+        bottle1_2 = self.bottle1_2(bottle1_1) + bottle1_1
+
+        up2 = self.up2(bottle1_2)
+        bottle2 = self.bottle2(up2) + up2
+
+        up3 = self.up3(bottle2)
+        out = self.conv_out(up3)
+
+        return out
+    
+    def bottleneck_block(self, in_channels):
+
+        block = nn.Sequential(
+                            torch.nn.Conv2d(in_channels, in_channels // 2, kernel_size=1, stride=1, padding=0),
+                            torch.nn.BatchNorm2d(in_channels // 2),
+                            torch.nn.ReLU(),
+                            torch.nn.Conv2d(in_channels // 2, in_channels, kernel_size=3, stride=1, padding=1),
+                            torch.nn.BatchNorm2d(in_channels),
+                            torch.nn.ReLU()
+                            )
+        return block
+
+
+
+model = RestorationDecoder()
+random_tensor = torch.randn(2, 256, 80, 80) # b c h w
+r = model(random_tensor)
+print("Input shape:", random_tensor.shape)
+print("Output shape:", r.shape)
+
+
+import json
+from pathlib import Path
+
+def read_json(filename):
+    data_dict = None
+    with open(filename, 'r', encoding='utf-8') as f:
+        try:
+            data_dict = json.load(f)
+        except:
+            pass
+
+    return data_dict
+
+
+def write_json(data_dict, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data_dict, f, indent=4, ensure_ascii=False)
+
+
+def checkpoint_load_path(path):
+    '''
+    path - str. Path of checkpoint
+    returns path or backup path based on saved json.
+    '''
+    p = Path(path)
+    info = Path(p.parent / (p.stem + '.json'))
+    if not info.exists():
+        return path
+
+    checkpoint_info = read_json(info)
+    if checkpoint_info is None: # JSON corruption means both checkpoints are good
+        return path
+
+    if checkpoint_info['backup_latest']:
+        return p.parent / (p.stem + '.back')
+    else:
+        return path
+
+def checkpoint_save_path(path, save_json=False):
+    '''
+    path - str. Path of checkpoint
+    returns path or backup path based on saved json.
+        JSON save must be done AFTER checkpoint saving
+    '''
+    p = Path(path)
+    save_to_back = False
+
+    info = Path(p.parent / (p.stem + '.json'))
+    if info.exists():
+        checkpoint_info = read_json(info)
+        if not checkpoint_info is None:
+            # JSON corruption means both checkpoints are good. Assume JSON is fine
+            if not checkpoint_info['backup_latest']:
+                # Latest checkpoint is not backup
+                save_to_back = True
+
+    if save_json:
+        write_json({'backup_latest': save_to_back}, info)
+
+    if not save_to_back:
+        return path
+    else:
+        return p.parent / (p.stem + '.back')
+
+
+
+
+import time
+
+def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, epochs=1):
+    start = time.time()
+    model.to(DEVICE)
+
+    train_loss, valid_loss = [], []
+
+    best_acc = 0.0
+
+    for epoch in range(epochs):
+        print('Epoch {}/{}'.format(epoch, epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train']:
+            if phase == 'train':
+                model.train(True)  # Set trainind mode = true
+                dataloader = train_dl
+            else:
+                model.train(False)  # Set model to evaluate mode
+                dataloader = valid_dl
+
+            running_loss = 0.0
+            running_acc = 0.0
+
+            step = 0
+
+            # iterate over data
+            for x, y in dataloader:
+                x = x.to(DEVICE)
+                y = y.to(DEVICE)
+                step += 1
+
+                # forward pass
+                if phase == 'train':
+                    # zero the gradients
+                    optimizer.zero_grad()
+                    outputs = model(x)
+                    loss = loss_fn(outputs, y)
+
+                    # the backward pass frees the graph memory, so there is no 
+                    # need for torch.no_grad in this training pass
+                    loss.backward()
+                    optimizer.step()
+                    # scheduler.step()
+
+                else:
+                    with torch.no_grad():
+                        outputs = model(x)
+                        loss = loss_fn(outputs, y.long())
+
+                # stats - whatever is the phase
+                acc = acc_fn(outputs, y)
+
+                running_acc  += acc*dataloader.batch_size
+                running_loss += loss*dataloader.batch_size
+
+                if step % 10 == 0:
+                    # clear_output(wait=True)
+                    print('Current step: {}  Loss: {}  Acc: {}  AllocMem (Mb): {}'.format(step, loss, acc, torch.cuda.memory_allocated()/1024/1024))
+                    #print(torch.cuda.memory_summary())
+
+                    # Save model
+                    model_path = Path("checkpoints/restore_model.pth")
+                    PATH = checkpoint_save_path(model_path)
+
+                    checkpoint = {
+                        'model': model
+                    }
+                    torch.save(checkpoint, PATH)
+                    checkpoint_save_path(PATH, save_json=True)
+
+                    pred = outputs.detach()[0].permute(1,2,0).cpu().numpy()
+                    outputs_dir = Path("outputs")
+                    cv2.imwrite(str(outputs_dir / f"{step}_pred.jpg"), (pred / (pred.max()+0.001) * 255).astype(np.uint8))
+
+            epoch_loss = running_loss / len(dataloader.dataset)
+            epoch_acc = running_acc / len(dataloader.dataset)
+
+            #clear_output(wait=True)
+            # print('Epoch {}/{}'.format(epoch, epochs - 1))
+            # print('-' * 10)
+            print('{} Loss: {:.4f} Acc: {}'.format(phase, epoch_loss, epoch_acc))
+            print('-' * 10)
+
+            train_loss.append(epoch_loss) if phase=='train' else valid_loss.append(epoch_loss)
+
+    time_elapsed = time.time() - start
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))    
+    
+    return train_loss, valid_loss
+
+model_dir = Path("checkpoints")
+model_dir.mkdir(parents=True, exist_ok=True)
+
+outputs_dir = Path("outputs")
+outputs_dir.mkdir(parents=True, exist_ok=True)
+
+
+
+
+model = RestorationDecoder()
+print("Parameters:", sum(p.numel() for p in model.parameters()))
+
+train_dataset = RestorationDataset()
+train_loader = data.DataLoader(train_dataset, batch_size=4, 
+        pin_memory=False, shuffle=True, num_workers=2, drop_last=True)
+
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+# train_loss, valid_loss = train(model, train_loader, None, loss_fn, optimizer, loss_fn, epochs=20)
+
+# print(train_loss)
+
+# speed: ~10 sec per 100 images 

697302041ecd37e9616ce3a0ec5b9996721c82e2
Max Velikanov <maximelianos.m@gmail.com>
2022-02-01 21:57:09 +0300

    1

--- yolov3-master/cut_gray_lines.py
+++ yolov3-master/cut_gray_lines.py
@@ -30,9 +30,7 @@ def cut_and_save(settings, result_dir):
     # 5000 files in each folder
 
     for idx in range(len(gt_files)):
-        print(type(gt_files[idx]))
-        print( Path(gt_files[idx]).exists() )
-        gt = cv2.imread(gt_files[idx]).astype(np.float32) / 255
+        gt = cv2.imread(str(gt_files[idx])).astype(np.float32) / 255
         h, w = gt.shape[:2]
 
         # count variance by row

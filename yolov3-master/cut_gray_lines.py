0bed3069cfea28de959979094bef9dbc65600f93
Max Velikanov <maximelianos.m@gmail.com>
2022-02-02 06:30:53 +0300

    1

--- yolov3-master/cut_gray_lines.py
+++ yolov3-master/cut_gray_lines.py
@@ -14,6 +14,60 @@ import torch
 import torch.utils.data as data
 from torch import nn
 
+
+def single_process(settings, result_dir, idx):
+    gt = cv2.imread(str(gt_files[idx]))
+    gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255
+    out_image = cv2.imread(str(out_files[idx])).astype(np.float32) / 255
+    coco_gt = cv2.imread(str(coco_files[idx])).astype(np.float32) / 255
+    h, w = gt.shape
+
+    # Count variance by row (top/bottom strips).
+    # var = avg( (x_i - avg(x))^2 )
+    # Variance works very bad!
+    # Use sobel + difference from gray
+    solid_gray = 0.44705883
+    sobel_thr = 1e-5
+    gray_thr = 1e-5
+
+    sobel = gt[:, 1:] - gt[:, :w - 1]
+    variance = np.average(sobel ** 2, axis=1)
+    gray_diff = np.average((gt - solid_gray) ** 2, axis=1)
+    # print(gray_diff)
+    high_var = gray_diff > gray_thr
+    grid = np.arange(h)
+    min_h = grid[high_var].min()
+    max_h = grid[high_var].max() + 1
+    if np.sum(~high_var[min_h:max_h]) != 0:
+        print("Strip detection error on image", idx)
+    # print(f"image {str(gt_files[idx])} min {min_w} max {max_w} minvar {np.min(variance)} maxvar {np.max(variance)}")
+    # cv2.imwrite(f"{idx:05d}_cut.jpg", np.clip(gt * 255, 0, 255).astype(np.uint8),
+    #             [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+    # count variance by column (left/right strips)
+    sobel = gt[1:, :] - gt[:h - 1, :]
+    variance = np.average(sobel ** 2, axis=0)
+    gray_diff = np.average((gt - solid_gray) ** 2, axis=0)
+    high_var = gray_diff > gray_thr
+    grid = np.arange(w)
+    min_w = grid[high_var].min()
+    max_w = grid[high_var].max() + 1
+    if np.sum(~high_var[min_w:max_w]) != 0:
+        print("Strip detection error on image", idx)
+
+    res_h, res_w = max_h - min_h, max_w - min_w
+    coco_h, coco_w = coco_gt.shape[:2]
+    if (res_h / res_w - coco_h / coco_w) ** 2 > 0.001:
+        print("Aspect ratio violation on image", idx, "coco", str(coco_files[idx]))
+
+    res_image = out_image[min_h:max_h, min_w:max_w]
+    res_path = str(result_dir / f"{idx:05d}_cut.jpg")
+    # cv2.imwrite(str(result_dir / coco_files[idx].name), np.clip(res_image * 255, 0, 255).astype(np.uint8),
+    #             [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+    if idx % 500 == 0:
+        print(idx)
+
 def cut_and_save(settings, result_dir):
     # gt
     # ├─00000
@@ -32,63 +86,10 @@ def cut_and_save(settings, result_dir):
     coco_files = sorted(Path(settings.coco_path).rglob("*.jpg"))
     # 5000 files in each folder
 
-    def single_process(idx):
-        gt = cv2.imread(str(gt_files[idx]))
-        gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255
-        out_image = cv2.imread(str(out_files[idx])).astype(np.float32) / 255
-        coco_gt = cv2.imread(str(coco_files[idx])).astype(np.float32) / 255
-        h, w = gt.shape
-
-        # Count variance by row (top/bottom strips).
-        # var = avg( (x_i - avg(x))^2 )
-        # Variance works very bad!
-        # Use sobel + difference from gray
-        solid_gray = 0.44705883
-        sobel_thr = 1e-5
-        gray_thr = 1e-5
-
-        sobel = gt[:, 1:] - gt[:, :w-1]
-        variance = np.average(sobel ** 2, axis=1)
-        gray_diff = np.average((gt - solid_gray) ** 2, axis=1)
-        #print(gray_diff)
-        high_var = gray_diff > gray_thr
-        grid = np.arange(h)
-        min_h = grid[high_var].min()
-        max_h = grid[high_var].max() + 1
-        if np.sum(~high_var[min_h:max_h]) != 0:
-            print("Strip detection error on image", idx)
-        # print(f"image {str(gt_files[idx])} min {min_w} max {max_w} minvar {np.min(variance)} maxvar {np.max(variance)}")
-        # cv2.imwrite(f"{idx:05d}_cut.jpg", np.clip(gt * 255, 0, 255).astype(np.uint8),
-        #             [cv2.IMWRITE_JPEG_QUALITY, 100])
-
-        # count variance by column (left/right strips)
-        sobel = gt[1:, :] - gt[:h - 1, :]
-        variance = np.average(sobel ** 2, axis=0)
-        gray_diff = np.average((gt - solid_gray) ** 2, axis=0)
-        high_var = gray_diff > gray_thr
-        grid = np.arange(w)
-        min_w = grid[high_var].min()
-        max_w = grid[high_var].max() + 1
-        if np.sum(~high_var[min_w:max_w]) != 0:
-            print("Strip detection error on image", idx)
-
-        res_h, res_w = max_h - min_h, max_w - min_w
-        coco_h, coco_w = coco_gt.shape[:2]
-        if (res_h/res_w - coco_h/coco_w) ** 2 > 0.001:
-            print("Aspect ratio violation on image", idx, "coco", str(coco_files[idx]))
-
-        res_image = out_image[min_h:max_h, min_w:max_w]
-        res_path = str(result_dir / f"{idx:05d}_cut.jpg")
-        # cv2.imwrite(str(result_dir / coco_files[idx].name), np.clip(res_image * 255, 0, 255).astype(np.uint8),
-        #             [cv2.IMWRITE_JPEG_QUALITY, 100])
-
-        if idx % 500 == 0:
-            print(idx)
-
     # Step 1: Init multiprocessing.Pool()
     pool = mp.Pool(mp.cpu_count())
     # Step 2: pool.apply
-    results = [pool.apply(single_process, args=(idx)) for idx in range(len(gt_files))]
+    results = [pool.apply(single_process, args=(settings, result_dir, idx)) for idx in range(len(gt_files))]
     # Step 3: Don't forget to close
     pool.close()
 

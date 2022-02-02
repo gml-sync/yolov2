8d2b0dff5966429d5c60c61bb8b0316038a854f0
Max Velikanov <maximelianos.m@gmail.com>
2022-02-02 03:05:30 +0300

    1

--- yolov3-master/cut_gray_lines.py
+++ yolov3-master/cut_gray_lines.py
@@ -42,6 +42,7 @@ def cut_and_save(settings, result_dir):
         high_var = variance > 0.008
         min_w = np.argmin(high_var)
         max_w = np.argmax(high_var) + 1
+        print(~high_var)
         print(np.sum((~high_var)[min_w:max_w]))
         break
         min_w = 0 # cut image in [min_w; max_w)

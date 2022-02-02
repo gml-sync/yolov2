e665c0c824d51289b08207bce3ca37bbb4729c5d
Max Velikanov <maximelianos.m@gmail.com>
2022-02-03 02:25:45 +0300

    1

--- yolov3-master/polus_scripts/ssh-upload.sh
+++ yolov3-master/polus_scripts/ssh-upload.sh
@@ -1,7 +1,10 @@
 rel_path=$(dirname "$0")                # relative
 abs_path=$(cd "$rel_path" && pwd)       # absolutized and normalized
 
-cd "$rel_path/.." && source polus_scripts/credentials.sh && sshpass -e sftp -oBatchMode=no -b - 23m_vel@calypso.gml-team.ru << !
+echo $SSHPASS
+cd "$rel_path/.." && source polus_scripts/credentials.sh
+echo $SSHPASS
+sshpass -e sftp -oBatchMode=no -b - 23m_vel@calypso.gml-team.ru << !
    cd work/22k_koz/recognition-aware/datasets
    put $1
    bye

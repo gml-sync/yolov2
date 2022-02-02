ce8c923d86da9412330889e0e490a32e1680d24f
Max Velikanov <maximelianos.m@gmail.com>
2022-02-02 04:29:52 +0300

    1

--- yolov3-master/cut_gray_lines.py
+++ yolov3-master/cut_gray_lines.py
@@ -49,9 +49,9 @@ def cut_and_save(settings, result_dir):
         #print(gray_diff)
         high_var = gray_diff > gray_thr
         grid = np.arange(h)
-        min_w = grid[high_var].min()
-        max_w = grid[high_var].max() + 1
-        if np.sum(~high_var[min_w:max_w]) != 0:
+        min_h = grid[high_var].min()
+        max_h = grid[high_var].max() + 1
+        if np.sum(~high_var[min_h:max_h]) != 0:
             print("Strip detection error on image", idx)
         # print(f"image {str(gt_files[idx])} min {min_w} max {max_w} minvar {np.min(variance)} maxvar {np.max(variance)}")
         # cv2.imwrite(f"{idx:05d}_cut.jpg", np.clip(gt * 255, 0, 255).astype(np.uint8),
@@ -63,9 +63,9 @@ def cut_and_save(settings, result_dir):
         gray_diff = np.average((gt - solid_gray) ** 2, axis=0)
         high_var = gray_diff > gray_thr
         grid = np.arange(w)
-        min_h = grid[high_var].min()
-        max_h = grid[high_var].max() + 1
-        if np.sum(~high_var[min_h:max_h]) != 0:
+        min_w = grid[high_var].min()
+        max_w = grid[high_var].max() + 1
+        if np.sum(~high_var[min_w:max_w]) != 0:
             print("Strip detection error on image", idx)
 
         res_image = gt[min_h:max_h, min_w:max_w]
